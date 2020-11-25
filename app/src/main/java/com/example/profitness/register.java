1e66c9fd93a59ff1a1235df98043b720bb5df52d
amichaihadad <amichaiaaa@gmail.com>
2020-11-25 20:27:15 +0200

    add registe not done

--- app/src/main/java/com/example/profitness/register.java
+++ app/src/main/java/com/example/profitness/register.java
@@ -1,20 +1,30 @@
 package com.example.profitness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class register extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class register extends AppCompatActivity implements View.OnClickListener {
     EditText mFullName, mEmail, mPassword;
     Button mRegisterBtn;
-    TextView mLoginBtn;
-    FirebaseAuth fAuth;
+    FirebaseAuth mAuth;
     ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +35,48 @@ public class register extends AppCompatActivity {
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
         mRegisterBtn = findViewById(R.id.btn_register);
-        mLoginBtn = findViewById(R.id.btn_login);
-        fAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+        mRegisterBtn.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v==mRegisterBtn){
+            mAuth.createUserWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("create user", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(mFullName.getText().toString())
+                                        .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
+                                        .build();
+                                assert user != null;
+                                user.updateProfile(profileUpdates)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.d("updateUser", "User profile updated.");
+                                                }
+                                            }
+                                        });
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("fail create", "createUserWithEmail:failure", task.getException());
+                                //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                //      Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+        }
+    }
 }
