08633f2ed27e762b2b4f90852d2193252d965f8f
Omer Kalif <omer908@gmail.com>
2020-11-25 18:19:08 +0200

    trainee_main_activity new page

---
+++ app/src/main/java/com/example/profitness/trainee_main_activity.java
@@ -0,0 +1,14 @@
+package com.example.profitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trainee_main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainee_main_activity);
+    }
+}
