22bb042f519f2d487507e4dab0baeaec06af56d3
amichaihadad <amichaiaaa@gmail.com>
2020-11-29 18:42:47 +0200

    add image

--- app/src/main/java/com/example/profitness/SchedAvailability.java
+++ app/src/main/java/com/example/profitness/SchedAvailability.java
@@ -31,6 +31,7 @@ import java.util.Map;
 
 public class SchedAvailability extends AppCompatActivity implements View.OnClickListener {
 
+
     String dateString,timeString;
     Button pick_btn,pickStartH,pickEndH,send;
     TextView showDate,showStartH,showEndH;
