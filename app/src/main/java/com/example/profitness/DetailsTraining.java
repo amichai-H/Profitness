de83d2deaa5a46f4ca8b5536fd46ad089852fff8
amichaihadad <amichaiaaa@gmail.com>
2020-12-06 20:18:00 +0200

    add name to trainee coch trainee show

--- app/src/main/java/com/example/profitness/DetailsTraining.java
+++ app/src/main/java/com/example/profitness/DetailsTraining.java
@@ -1,15 +1,29 @@
 package com.example.profitness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DetailsTraining extends AppCompatActivity implements View.OnClickListener {
     private Button editMenu;
     String uid;
+    FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    TextView showTraineeName;
 
 
     @Override
@@ -19,9 +33,18 @@ public class DetailsTraining extends AppCompatActivity implements View.OnClickLi
 
         editMenu = (Button)findViewById(R.id.menuBtn);
         uid = (String) getIntent().getExtras().get("Uid");
+        showTraineeName = findViewById(R.id.showTrainneName);
 
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         editMenu.setOnClickListener(this);
+        updateUI();
+    }
+
+    private void updateUI() {
+        setUserName();
+
     }
 
     @Override
@@ -31,5 +54,32 @@ public class DetailsTraining extends AppCompatActivity implements View.OnClickLi
             intent.putExtra("Uid", uid);
             startActivity(intent);
         }
+    }
+    private void setUserName() {
+        DocumentReference docRef = db.collection("users").document(uid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    //mUser = new MyUser((String)document.getData().get("first"), (String)document.getData().get("last"), );
+                    String userFirstNameString = (String)document.getData().get("first");
+                    String userLastNameString = (String)document.getData().get("last");
+                    showTraineeName.setText("Trainee " + userFirstNameString + " " + userLastNameString);
+
+                    if (document.exists()) {
+                        Log.d("readData", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d("notFound", "No such document");
+                        String e404 = "Error 404 user not found";
+                        showTraineeName.setText(e404);
+                    }
+                } else {
+                    Log.d("NotConnected", "get failed with ", task.getException());
+                    String e502 = "Error 502 failed to connect to server";
+                    showTraineeName.setText(e502);
+                }
+            }
+        });
     }
 }
